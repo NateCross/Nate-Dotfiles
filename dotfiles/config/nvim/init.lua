4c27c7da79d4f2f36fc44643cd85c4db82091f36
Nathan Angelo B. Cruz <jonathan.a.b.cross@gmail.com>
2023-01-31 00:33:09 +0800

    switch to lazynvim

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,16 +1,2 @@
--- Nate's personal config
--- Uses packer.nvim
--- You can use the command "gf" while hovering over
--- the commented file directory to jump to that file
-
--- Plugins
--- lua/plugins.lua
-require('plugins')
-
--- Keybinds
--- lua/keybinds.lua
-require('keybinds')
-
--- Preferences
--- lua/prefs.lua
-require('prefs')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
